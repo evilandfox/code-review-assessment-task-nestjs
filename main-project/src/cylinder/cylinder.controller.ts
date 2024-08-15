4268047186fdbbf11f66dca24479704733e7a341
evilandfox <evilandfox@gmail.com>
2024-08-15 22:47:27 +0500

    feat: input validation fail response

--- main-project/src/cylinder/cylinder.controller.ts
+++ main-project/src/cylinder/cylinder.controller.ts
@@ -1,4 +1,10 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpException,
+  Param,
+  ParseFloatPipe,
+} from '@nestjs/common';
 import { CylinderService } from './cylinder.service';
 
 @Controller('cylinder')
@@ -7,8 +13,20 @@ export class CylinderController {
 
   @Get(':radius/:height')
   async calculateArea(
-    @Param('radius') radius: number,
-    @Param('height') height: number,
+    @Param(
+      'radius',
+      new ParseFloatPipe({
+        exceptionFactory: () => new HttpException('Радиус не число', 400),
+      }),
+    )
+    radius: number,
+    @Param(
+      'height',
+      new ParseFloatPipe({
+        exceptionFactory: () => new HttpException('Длина не число', 400),
+      }),
+    )
+    height: number,
   ) {
     return this.cylinderService.calculateArea(radius, height);
   }
