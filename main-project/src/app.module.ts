59733eba7717299bb1d0fc24bdccab314cf73343
evilandfox <evilandfox@gmail.com>
2024-08-15 21:57:55 +0500

    fix: metrics

--- main-project/src/app.module.ts
+++ main-project/src/app.module.ts
@@ -3,9 +3,7 @@ import { APP_FILTER } from '@nestjs/core';
 import { PromModule } from '@digikare/nestjs-prom';
 import { CylinderModule } from './cylinder/cylinder.module';
 import { MyNatsMiddleware } from './middleware/nats.middleware';
-import { MetricsProvider } from './providers/metrics.provider';
 import { MyExceptionFilter } from './exception/exception.filter';
-import { Registry } from 'prom-client';
 
 @Module({
   imports: [
@@ -17,16 +15,14 @@ import { Registry } from 'prom-client';
     CylinderModule,
   ],
   providers: [
-    ...MetricsProvider,
     {
       provide: APP_FILTER,
       useClass: MyExceptionFilter,
     },
-    Registry,
   ],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
-    consumer.apply(MyNatsMiddleware).forRoutes('*');
+    consumer.apply(MyNatsMiddleware).forRoutes('cylinder');
   }
 }
--- main-project/src/middleware/nats.middleware.ts
+++ main-project/src/middleware/nats.middleware.ts
@@ -1,50 +1,47 @@
+import {
+  CounterMetric,
+  PromService,
+  SummaryMetric,
+} from '@digikare/nestjs-prom';
 import { Injectable, NestMiddleware } from '@nestjs/common';
-import { Counter, Summary, Registry } from 'prom-client';
 
 @Injectable()
 export class MyNatsMiddleware implements NestMiddleware {
-  private readonly messagesCounter: Counter<string>;
-  private readonly processingTimeSummary: Summary<string>;
-  private readonly statusCounter: Counter<string>;
+  private readonly messagesCounter: CounterMetric;
+  private readonly processingTimeSummary: SummaryMetric;
+  private readonly statusCounter: CounterMetric;
 
-  constructor(private readonly registry: Registry) {
-    this.messagesCounter = new Counter({
+  constructor(private readonly promService: PromService) {
+    this.messagesCounter = this.promService.getCounter({
       name: 'nats_messages_counter',
-      help: 'NATS messages counter',
-      labelNames: ['messages_counter'],
-      registers: [this.registry],
+      help: 'Total number of NATS messages',
+      labelNames: []
     });
 
-    this.processingTimeSummary = new Summary({
+    this.processingTimeSummary = this.promService.getSummary({
       name: 'nats_messages_processing_time',
-      help: 'NATS messages processing time',
-      labelNames: ['messages_processing_time'],
-      registers: [this.registry],
+      help: 'Processing time of NATS messages',
+      labelNames: []
     });
 
-    this.statusCounter = new Counter({
+    this.statusCounter = this.promService.getCounter({
       name: 'nats_messages_status_counter',
-      help: 'NATS messages status counter',
-      labelNames: ['messages_status_counter', 'statusCode'], // Add the "statusCode" label here
-      registers: [this.registry],
+      help: 'Status codes of NATS message responses',
+      labelNames: ['status'],
     });
   }
 
   use(req: any, res: any, next: () => void) {
     const start = process.hrtime();
 
-    this.messagesCounter.inc({ messages_counter: 'value' });
+    this.messagesCounter.inc();
 
     res.on('finish', () => {
       const duration = process.hrtime(start);
       const processingTime = duration[0] * 1000 + duration[1] / 1e6;
-      this.processingTimeSummary.observe(
-        { messages_processing_time: 'value' },
-        processingTime,
-      );
+      this.processingTimeSummary.observe(processingTime);
       this.statusCounter.inc({
-        messages_status_counter: 'value',
-        statusCode: res.statusCode.toString(),
+        status: res.statusCode.toString(),
       });
     });
 
